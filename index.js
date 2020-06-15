7b983c1de5ee997572a391c9968d96ec33b03b63
ShepherdNg <hk7math@gmail.com>
2020-06-15 09:11:02 +0800

    3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json())
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 app.use(express.static('build'))
 
-let persons
-
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons)
@@ -30,21 +28,27 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  Person.find({_id: req.params.id}).then(persons => {
-    const person = persons[0]
-    if (person){
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
-  
+  Person.find({_id: req.params.id})
+    .then(persons => {
+      const person = persons[0]
+      if (person){
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id|0
-  persons = persons.filter(p => p.id!==id)
-  res.status(204).end()
+  Person.deleteOne({_id: req.params.id})
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => {
+      res.status(400).json({
+        error: err
+      })
+    })  
 })
 
 app.post('/api/persons', (req, res) => {
@@ -54,10 +58,6 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({
       error: 'name missing'
     })
-  } else if (persons.some(person => person.name === body.name)) {
-    return res.status(403).json({
-      error: 'duplicated name'
-    })
   }
   
   if (!body.number) {
@@ -66,14 +66,30 @@ app.post('/api/persons', (req, res) => {
     })
   }
   
-  const person = {
+  Person.find({name: body.name})
+  .then(persons => {
+    if (persons.length > 0){
+        return res.status(403).json({
+          error: 'duplicated name'
+        })
+      }
+    })
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random()*1e6)
-  }
+  })
 
-  persons = persons.concat(person)
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+})
+
+app.put('/api/persons/:id', (req, res) => {
+  Person.updateOne({_id: req.params.id}, {number: req.body.number})
+    .then(updatedRes => {
+      res.json(req.body)
+    })
 })
 
 const PORT = process.env.PORT
