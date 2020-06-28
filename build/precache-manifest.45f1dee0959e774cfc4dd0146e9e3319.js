614e9ffda071ae9a3d16e98e53e38f032ff88447
ShepherdNg <hk7math@gmail.com>
2020-06-28 21:42:54 +0800

    3.20*: Phonebook database, step8

---
+++ build/precache-manifest.45f1dee0959e774cfc4dd0146e9e3319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bf8059a401e3a86b8e8188254317e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2a03ca040f8d7413be",
+    "url": "/static/js/2.94ca2cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ca2cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dda2197f278a27b8e09",
+    "url": "/static/js/main.57a63b14.chunk.js"
+  },
+  {
+    "revision": "6b189a97c36646ef317f",
+    "url": "/static/js/runtime-main.2c31142c.js"
+  }
+]);
